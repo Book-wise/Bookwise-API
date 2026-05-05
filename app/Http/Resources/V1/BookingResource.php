0c5f214c369399d9f6348231bf09ae124a4f89d6
Sebastian <sebaqd91@gmail.com>
2026-05-05 22:25:40 +0100

    fix: booking without sale returns payment_status 'unpaid' instead of null

--- app/Http/Resources/V1/BookingResource.php
+++ app/Http/Resources/V1/BookingResource.php
@@ -30,7 +30,7 @@ class BookingResource extends JsonResource
                 'color'           => $this->status?->color,
                 'is_cancellation' => $this->status?->is_cancellation,
             ],
-            'payment_status'             => $this->whenLoaded('sale', fn() => $this->sale->payment_status, null),
+            'payment_status'             => $this->whenLoaded('sale', fn() => $this->sale->payment_status, 'unpaid'),
             'payment'                    => new PaymentResource($this->whenLoaded('sale')),
             'pack_session'               => $this->whenLoaded('packSession', function () {
                 $ps = $this->packSession;
